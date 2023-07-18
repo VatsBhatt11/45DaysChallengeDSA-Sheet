c9000c349ddb2646c07e912ad7f9262fcf9c7029
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-07-18 23:55:17 +0530

    Commit

---
+++ LinkedList/Binary No to Decimal LL.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        ListNode* temp=head;
+        int ans=0;
+        while(temp)
+        {
+            ans=ans<<1;
+            ans+=temp->val;
+            temp=temp->next;
+        }
+        return ans;
+    }
+};
