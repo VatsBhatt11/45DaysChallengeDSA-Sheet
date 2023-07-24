a3f17633b529725ebce2fc86f9d84bda371e589f
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-07-24 21:55:36 +0530

    Commit

---
+++ Trees/Count BST Nodes that lie in a given Range.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    int getCount(Node *root, int l, int h)
+    {
+        if(!root)
+            return 0;
+        if(root->data<=h && root->data>=l)
+            return 1+getCount(root->left,l,h)+getCount(root->right,l,h);
+        else if(root->data<l)
+            return getCount(root->right,l,h);
+        return getCount(root->left,l,h);
+    }
+};
