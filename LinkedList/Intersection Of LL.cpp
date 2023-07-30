22219c3b8eab2a0244e6b44cef11bd1a22b28fe2
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-07-30 23:26:31 +0530

    Commit

---
+++ LinkedList/Intersection Of LL.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* l1=headA,*l2=headB;
+        while(l1 != l2)
+        {
+            if(l1)
+                l1=l1->next;
+            else
+                l1=headB;
+            if(l2)
+                l2=l2->next;
+            else
+                l2=headA;
+        }
+        return l1;
+    }
+};
